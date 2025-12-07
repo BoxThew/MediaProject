d56e67739f29d58fd08f095075a4e59163adfc70
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2025-12-07 15:32:32 -0800

    created pair of key values for artist and album

--- include/Database.hpp
+++ include/Database.hpp
@@ -5,13 +5,32 @@
 #include <memory>
 #include "Song.hpp"
 #include <map>
+#include <unordered_map>
+#include <string> 
+
+
+struct ArtistAlbumKey{
+    std::string artist;
+    std::string album; 
+
+    //check objects
+    bool operation==(const ArtistAlbumKey& other) const{
+        return artist == other.artists && album other.album; 
+    }
+
+};
+
+struct ArtistAlbumKeyHash{
+    //TODO: define a hash function
+}// 
+
 
 class Database{
 
     static std::vector<std::unique_ptr<Song>> songs;
     static std::map<std::string, std::vector<Song*>> songs_by_artists;
     static std::map<std::string, std::vector<Song*>> songs_in_album;
-
+    static std::unordered_map<ArtistAlbumKey, std::vector<Song*> ArtistAlbumKeyHash>songs_by_artists_album; 
 
     static bool artist_exists(const std::string& artist);
     static bool album_exists(const std::string& album);
--- src/Data/Database.cpp
+++ src/Data/Database.cpp
@@ -3,29 +3,35 @@
 #include "Song.hpp"
 #include <vector>
 #include <memory>
+#include <unordered_map>
 #include <map>
 #include <string>
 
 std::vector<std::unique_ptr<Song>> Database::songs{};
 std::map<std::string, std::vector<Song*>> Database::songs_by_artists{};
 std::map<std:: string, std::vector<Song*>> Database::songs_in_album{};
+std::unordered_map<ArtistAlbumKey, std::vector<Song*>, ArtistAlbumKeyHash> Database::songs_by_artists_album; 
+
+
 
 bool Database::artist_exists(const std::string& artist){
     return songs_by_artists.find(artist) != songs_by_artists.end();
 }
 
 
-//need to implement this
 bool Database::album_exists(const std::string& album){
-    //FIXME
-
-    return false;
+    return songs_in_album.find(album) != songs_in_album.end(); 
 }
+
+bool Database::songs_by_artists_album_exists(const std::string& artist, const std::string& album){
+    ArtistAlbumKey key(artist, album); 
+    return songs_by_artists_album.find(key) != songs_by_artists_album.end(); 
+}
+
 
 Database::Database(){
 
 }
-
 
 void Database::add_song(const Song& song){
     std::unique_ptr<Song> pSong = std::make_unique<Song>(song);
@@ -44,9 +50,9 @@ void Database::add_song(const Song& song){
 
 
 
-
 void Database::remove_song(const Song& song){
-    //to do
+//TODO
+    
 }
 
 
