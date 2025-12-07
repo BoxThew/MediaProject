87c2dd73d130a4a86d249f14c651ca971d352863
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2025-12-07 15:46:33 -0800

    fixed formats

--- include/Database.hpp
+++ include/Database.hpp
@@ -22,7 +22,7 @@ struct ArtistAlbumKey{
 
 struct ArtistAlbumKeyHash{
     //TODO: define a hash function
-}// 
+};
 
 
 class Database{
@@ -30,7 +30,7 @@ class Database{
     static std::vector<std::unique_ptr<Song>> songs;
     static std::map<std::string, std::vector<Song*>> songs_by_artists;
     static std::map<std::string, std::vector<Song*>> songs_in_album;
-    static std::unordered_map<ArtistAlbumKey, std::vector<Song*>, ArtistAlbumKeyHash>songs_by_artists_album; 
+    static std::unordered_map<ArtistAlbumKey, std::vector<Song*>, ArtistAlbumKeyHash> songs_by_artists_album; 
 
     static bool artist_exists(const std::string& artist);
     static bool album_exists(const std::string& album);
