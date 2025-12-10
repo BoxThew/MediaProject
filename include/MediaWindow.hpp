2177bb6fef2c8bd1f589993406ca74a94071a9e6
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2025-12-10 11:15:14 -0800

    UI Buttons

--- include/MediaWindow.hpp
+++ include/MediaWindow.hpp
@@ -5,18 +5,33 @@
 #include "WindowADT.hpp"
 #include "SFML/Graphics/Font.hpp"
 # include "SFML/Graphics/Text.hpp"
+#include <SFML/Audio/Music.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include "Song.hpp"
 #include "Database.hpp"
 #include <vector>
 
-class MediaWindow : public WindowADT{
+class MediaWindow: public WindowADT{
 	sf::Font title_font;
 	sf::Text *song_title;
 	sf::Text *artist_text;
 
+	//song list
 	std::vector<Song*> songs;
 	int selected_index = -1; 
+	int first_visible_index = 0; 
 
+	//Buttons
+	sf::RectangleShape prev_button;
+	sf::RectangleShape play_button;
+	sf::RectangleShape next_button;
+
+	sf::Text prev_label;
+	sf::Text play_label;
+	sf::Text next_label; 
+
+	//Audio
+	sf::Music music; 
 
 
 	void display_song_title(const Song* song);
@@ -26,6 +41,8 @@ class MediaWindow : public WindowADT{
 	void display_song_album(const Song* song);
 
 	void center_text(sf::Text* text);
+
+	void play_selected_song(); 
 
 
 protected:
--- src/UI/MediaWindow.cpp
+++ src/UI/MediaWindow.cpp
@@ -1,26 +1,37 @@
 #include "MediaWindow.hpp"
 #include "SFML/Graphics/Text.hpp"
 #include "SFML/Graphics/Color.hpp"
+#include <SFML/Graphics/RectangleShape.hpp>
 #include "WindowADT.hpp"
 #include <iostream>
 #include "Database.hpp"
 
 
 
-MediaWindow::MediaWindow() : WindowADT("Media Window"){
+sf::Color white = sf::Color::White;
+sf::Color blue = sf::Color::Blue; 
+sf::Color red = sf::Color::Red; 
+
+
+MediaWindow::MediaWindow(): WindowADT("Media Window"), 
+song_title(nullptr),
+artist_text(nullptr),
+prev_label(title_font, "Prev", 18),
+play_label(title_font, "Play", 18),
+next_label(title_font, "Next", 18)
+{
 
 	if(!title_font.openFromFile("assets/Fonts/Sono-Bold.ttf")){
 		std::cout << "No Font found.\n";
 	}
 
-
 	song_title = new sf::Text(title_font,  " ", 24);
 	artist_text = new sf::Text(title_font, " ", 18); 
 
 	song_title->setPosition({SCREEN_WIDTH / 2, 600});
-	song_title->setFillColor(sf::Color::Red);
+	song_title->setFillColor(red);
 
-	artist_text->setFillColor(sf::Color::Red); 
+	artist_text->setFillColor(red); 
 	artist_text->setPosition({SCREEN_WIDTH / 2, 400});
 
 	//load all songs
@@ -28,13 +39,44 @@ MediaWindow::MediaWindow() : WindowADT("Media Window"){
 	for(auto& s: allSong){
 		songs.push_back(s.get()); 
 	}
+
+	//Buttons
+	sf::Vector2f buttonSize(100.0f, 40.0f); 
+	float bottomY = 740.f;
+	float labelY = bottomY + 8.0f;
+
+	prev_button.setSize(buttonSize);
+	play_button.setSize(buttonSize); 
+	next_button.setSize(buttonSize);
+
+	sf::Color buttonColor(100, 150, 200); 
+	prev_button.setFillColor(buttonColor);
+	play_button.setFillColor(buttonColor);
+	next_button.setFillColor(buttonColor);
+
+	prev_button.setPosition({150.0f, bottomY});
+	play_button.setPosition({350.0f, bottomY});
+	next_button.setPosition({550.0f, bottomY}); 
+
+	//Labels
+	prev_label.setString("Prev");
+	play_label.setString("Play");
+	next_label.setString("Next");
+	
+	prev_label.setFillColor(white);
+	play_label.setFillColor(white);
+	next_label.setFillColor(white);
+
+	prev_label.setPosition({160.0f, labelY});
+	play_label.setPosition({370.0f, labelY});
+	next_label.setPosition({570.0f, labelY});
 }
 
 void MediaWindow::handle_event(const sf::Event& event){
 
 	WindowADT::handle_event(event);
 
-
+	//Event: mouse clicks
 	if (auto* mouse = event.getIf<sf::Event::MouseButtonPressed>()){ 
 
 		if(mouse->button == sf::Mouse::Button::Left){ 
@@ -95,29 +137,44 @@ void MediaWindow::draw(){
 	if (artist_text){
 		window.draw(*artist_text);
 	}
-
+	//Draw: scrollable list
 	float x = 50.0f;
 	float y = 50.0f; 
 	float lineHeight = 24.0f; 
+	
 	sf::Text lineText(title_font,"", 18);
 
-	for(std::size_t i = 0; i< songs.size(); i++){
+	int visibleSongs = 10;
+	int start = first_visible_index;
+	int end = std::min(start + visibleSongs, static_cast<int>(songs.size()));
+
+	for(int i = 0; i < end; i++){
+
 		Song* s = songs[i];
+		
 		if(!s){
 			continue;
 		}
 		lineText.setString(s->get_title());
 		lineText.setPosition({x,y});
 
-		if(static_cast<int>(i) == selected_index){
-			lineText.setFillColor(sf::Color::Red); 
+		if(i == selected_index){
+			lineText.setFillColor(red); 
 		}
 		else{
-			lineText.setFillColor(sf::Color::Blue); 
+			lineText.setFillColor(blue); 
 		}
 		window.draw(lineText);
 		y += lineHeight; 
 	}
+
+	window.draw(prev_button);
+	window.draw(play_button);
+	window.draw(next_button);
+
+	window.draw(prev_label);
+	window.draw(play_label);
+	window.draw(next_label); 
 }
 
 
